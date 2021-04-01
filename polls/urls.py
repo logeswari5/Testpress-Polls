c15c06b2445a1f5ab70fe4ee6283f1ed3eea7992
HP <logeswari5>
2021-04-01 18:39:06 +0530

    Add URL in mysite urls.py

--- polls/urls.py
+++ polls/urls.py
@@ -22,5 +22,5 @@ urlpatterns = [
     path('<int:pk>/', views.DetailView.as_view(), name='detail'),
     path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
     path('<int:question_id>/vote/', views.vote, name='vote'),
-    url(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
+
 ]
